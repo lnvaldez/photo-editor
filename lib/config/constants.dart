1f6368ce9fc63ceebdf5110f06f2d86ba13a5732
Lucas Valdez <lnvaldez24@gmail.com>
2025-01-30 14:31:00 -0300

    Filter model

---
+++ lib/config/constants.dart
@@ -0,0 +1,6 @@
+class AppConstants {
+  static const String galleryPath = '/storage/emulated/0/DCIM/Camera';
+  static const int imageQuality = 90;
+  static const double filterThumbnailSize = 80;
+  static const double filterListHeight = 120;
+}
---
+++ lib/models/filter_option.dart
@@ -0,0 +1,9 @@
+class FilterOption {
+  final String name;
+  final List<double> matrix;
+
+  const FilterOption({
+    required this.name,
+    required this.matrix,
+  });
+}
